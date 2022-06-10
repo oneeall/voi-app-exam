73f102c1f7b72ab314c474db949c1e3cd82854f1
Danniel Alvianto <danniel.alvianto@machtwatch.co.id>
2022-06-10 19:12:14 +0700

    revert code

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runZonedGuarded(() {}, (Object error, StackTrace stackTrace) {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    FlutterError.onError = flutterOnError;
-    runApp(const MyApp());
-  });
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -58,15 +51,14 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    // setState(() {
-    //   // This call to setState tells the Flutter framework that something has
-    //   // changed in this State, which causes it to rerun the build method below
-    //   // so that the display can reflect the updated values. If we changed
-    //   // _counter without calling setState(), then the build method would not be
-    //   // called again, and so nothing would appear to happen.
-    //   _counter++;
-    // });
-    throw ArgumentError('testing error');
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
@@ -120,8 +112,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
-
-void flutterOnError(FlutterErrorDetails details) {
-  FlutterError.presentError(details);
 }
