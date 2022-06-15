8417899b4b3859ffeba7a33477382fcc604bacb0
alfacentaury <alfa.arnialfa@gmail.com>
2022-06-15 16:11:51 +0700

    Add text style and color

---
+++ lib/src/ui/themes/colors.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+/// color mw
+const kBlack = Color(0xFF000000);
+const kGrey = Color(0xFF595959);
+const kCommonColor = MaterialColor(0xFFF1AF3F, <int, Color>{});
+const kAccentRed = Color(0xFFF44336);
+
+/// red color
+const kRed1 = Color(0xFF3E0707);
+const kRed2 = Color(0xFF660909);
+const kRed3 = Color(0xFF930B0B);
+const kRed4 = Color(0xFFB71C1C);
+const kRed5 = Color(0xFFD82F23);
+const kRed6 = Color(0xFFF44336);
+const kRed7 = Color(0xFFFF5B4F);
+const kRed8 = Color(0xFFFF8279);
+const kRed9 = Color(0xFFFF9C95);
+const kRed10 = Color(0xFFFFCFCC);
+
+/// Volcano color
+const kVolcano1 = Color(0xFF30150E);
+const kVolcano2 = Color(0xFF4A1A0C);
+const kVolcano3 = Color(0xFF672812);
+const kVolcano4 = Color(0xFF8B3012);
+const kVolcano5 = Color(0xFFBF4014);
+const kVolcano6 = Color(0xFFE94C18);
+const kVolcano7 = Color(0xFFF26E39);
+const kVolcano8 = Color(0xFFFF9767);
+const kVolcano9 = Color(0xFFFFBF9C);
+const kVolcano10 = Color(0xFFFDD9C2);
+
+/// Orange color
+const kOrange1 = Color(0xFF652E00);
+const kOrange2 = Color(0xFF9F4901);
+const kOrange3 = Color(0xFFC65B03);
+const kOrange4 = Color(0xFFEF6C00);
+const kOrange5 = Color(0xFFF68522);
+const kOrange6 = Color(0xFFFFA230);
+const kOrange7 = Color(0xFFFFC069);
+const kOrange8 = Color(0xFFFFD591);
+const kOrange9 = Color(0xFFFFF7E6);
+const kOrange10 = Color(0xFFFFF7E6);
+
+/// Yellow color
+const kYellow1 = Color(0xFF322606);
+const kYellow2 = Color(0xFF4B390A);
+const kYellow3 = Color(0xFF694E09);
+const kYellow4 = Color(0xFF966B00);
+const kYellow5 = Color(0xFFD09500);
+const kYellow6 = Color(0xFFEDAB03);
+const kYellow7 = Color(0xFFF6BA22);
+const kYellow8 = Color(0xFFFFD54F);
+const kYellow9 = Color(0xFFFFE388);
+const kYellow10 = Color(0xFFFFEBB8);
+
+/// Sunrise color
+const kSunrise1 = Color(0xFF2C260F);
+const kSunrise2 = Color(0xFF463C0E);
+const kSunrise3 = Color(0xFF695E13);
+const kSunrise4 = Color(0xFF7C6E14);
+const kSunrise5 = Color(0xFFAA9514);
+const kSunrise6 = Color(0xFFD8BD14);
+const kSunrise7 = Color(0xFFE8D639);
+const kSunrise8 = Color(0xFFFCEC61);
+const kSunrise9 = Color(0xFFF8F48B);
+const kSunrise10 = Color(0xFFFAFAB5);
+
+/// Lime color
+const kLime1 = Color(0xFF1F2611);
+const kLime2 = Color(0xFF2e3c10);
+const kLime3 = Color(0xFF3e4f13);
+const kLime4 = Color(0xFF536d13);
+const kLime5 = Color(0xFF6f9412);
+const kLime6 = Color(0xFF8bbb11);
+const kLime7 = Color(0xFFa9d134);
+const kLime8 = Color(0xFFc9e75d);
+const kLime9 = Color(0xFFe4f88b);
+const kLime10 = Color(0xFFf0fab5);
+
+/// Green color
+const kGreen1 = Color(0xFF042E1B);
+const kGreen2 = Color(0xFF064126);
+const kGreen3 = Color(0xFF075331);
+const kGreen4 = Color(0xFF0B603A);
+const kGreen5 = Color(0xFF12784A);
+const kGreen6 = Color(0xFF08A45E);
+const kGreen7 = Color(0xFF05CB72);
+const kGreen8 = Color(0xFF5EE5A9);
+const kGreen9 = Color(0xFFACF5D5);
+const kGreen10 = Color(0xFFDFFFF1);
+
+/// Cyan color
+const kCyan1 = Color(0xFF112123);
+const kCyan2 = Color(0xFF084B46);
+const kCyan3 = Color(0xFF0C6A62);
+const kCyan4 = Color(0xFF178C82);
+const kCyan5 = Color(0xFF39A097);
+const kCyan6 = Color(0xFF4DB6AC);
+const kCyan7 = Color(0xFF52C9BD);
+const kCyan8 = Color(0xFF64DBD0);
+const kCyan9 = Color(0xFF80F0E5);
+const kCyan10 = Color(0xFFC0FFF9);
+
+/// Blue color
+const kBlue1 = Color(0xFF092342);
+const kBlue2 = Color(0xFF0C3361);
+const kBlue3 = Color(0xFF10427C);
+const kBlue4 = Color(0xFF1456A0);
+const kBlue5 = Color(0xFF1565C0);
+const kBlue6 = Color(0xFF1878E4);
+const kBlue7 = Color(0xFF2196F3);
+const kBlue8 = Color(0xFF64B5F6);
+const kBlue9 = Color(0xFF9CD3FF);
+const kBlue10 = Color(0xFFb7e3fa);
+
+/// Geek Blue color
+const kGeekBlue1 = Color(0xFF0E1333);
+const kGeekBlue2 = Color(0xFF0F1952);
+const kGeekBlue3 = Color(0xFF17246F);
+const kGeekBlue4 = Color(0xFF202F8A);
+const kGeekBlue5 = Color(0xFF293AA3);
+const kGeekBlue6 = Color(0xFF3949AB);
+const kGeekBlue7 = Color(0xFF5B6CD3);
+const kGeekBlue8 = Color(0xFF8694E9);
+const kGeekBlue9 = Color(0xFFB6C0FF);
+const kGeekBlue10 = Color(0xFFDEE3FF);
+
+/// Purple color
+const kPurple1 = Color(0xFF1a1325);
+const kPurple2 = Color(0xFF230932);
+const kPurple3 = Color(0xFF340E4A);
+const kPurple4 = Color(0xFF49136A);
+const kPurple5 = Color(0xFF581681);
+const kPurple6 = Color(0xFF6A1B9A);
+const kPurple7 = Color(0xFF9958C0);
+const kPurple8 = Color(0xFFBE8ADE);
+const kPurple9 = Color(0xFFE1BEE7);
+const kPurple10 = Color(0xFFF7EDFF);
+
+/// Magenta color
+const kMagenta1 = Color(0xFF291321);
+const kMagenta2 = Color(0xFF40162f);
+const kMagenta3 = Color(0xFF551c3b);
+const kMagenta4 = Color(0xFF75204f);
+const kMagenta5 = Color(0xFFa02669);
+const kMagenta6 = Color(0xFFcb2b83);
+const kMagenta7 = Color(0xFFe0529c);
+const kMagenta8 = Color(0xFFf37fb7);
+const kMagenta9 = Color(0xFFf8a8cc);
+const kMagenta10 = Color(0xFFfad2e3);
+
+
+///natural color
+const kNeutral1 = Color(0xFF111111);
+const kNeutral2 = Color(0xFF181818);
+const kNeutral3 = Color(0xFF242424);
+const kNeutral4 = Color(0xFF757575);
+const kNeutral5 = Color(0xFFBDBDBD);
+const kNeutral6 = Color(0xFFE0E0E0);
+const kNeutral7 = Color(0xFFEEEEEE);
+const kNeutral8 = Color(0xFFF5F5F5);
+const kNeutral9 = Color(0xFFFAFAFA);
+const kNeutral10 = Color(0xFFFFFFFF);
---
+++ lib/src/ui/themes/text_style.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_app/src/ui/themes/colors.dart';
+
+class TextThemeVoila {
+  late BuildContext localContext;
+
+  static TextThemeVoila? _current;
+
+  TextThemeVoila(BuildContext context) {
+    localContext = context;
+  }
+
+  static TextThemeVoila get current => _current!;
+
+  static TextThemeVoila init(BuildContext context) {
+    return _current ??= TextThemeVoila(context);
+  }
+
+  TextStyle get headline1 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .headline1!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 48,
+        color: kNeutral3,
+        height: 0.96,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0);
+  }
+
+  TextStyle get headline2 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .headline2!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 40,
+        color: kNeutral3,
+        height: 0.99,
+        fontWeight: FontWeight.bold,
+    letterSpacing: 0);
+  }
+
+  TextStyle get headline3 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .headline3!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 32,
+        color: kNeutral3,
+        fontWeight: FontWeight.bold,
+        height: 1.03,
+        letterSpacing: 0);
+  }
+
+  TextStyle get headline4 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .headline4!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 24,
+        color: kNeutral3,
+        fontWeight: FontWeight.bold,
+        height: 1.10,
+        letterSpacing: 0);
+  }
+
+  TextStyle get headline5 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .headline5!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 16,
+        color: kNeutral3,
+        fontWeight: FontWeight.bold,
+        height: 1.24,
+        letterSpacing: 0);
+  }
+
+  TextStyle get title {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .subtitle1!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 16,
+        color: kNeutral3,
+        fontWeight: FontWeight.w900,
+        height: 1.75,
+        letterSpacing: 0.1);
+  }
+
+  TextStyle get subtitle {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .subtitle2!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 14,
+        color: kNeutral3,
+        fontWeight: FontWeight.w900,
+        height: 1.71,
+        letterSpacing: 0.1);
+  }
+
+  TextStyle get body1 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .bodyText1!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 16,
+        color: kNeutral3,
+        fontWeight: FontWeight.normal,
+        height: 1.75,
+        letterSpacing: 0.5);
+  }
+
+  TextStyle get body2 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .bodyText2!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 14,
+        color: kNeutral3,
+        fontWeight: FontWeight.normal,
+        height: 1.71,
+        letterSpacing: 0.25);
+  }
+
+  TextStyle get body3 {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .bodyText2!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 12,
+        color: kNeutral3,
+        fontWeight: FontWeight.normal,
+        height: 1.66,
+        letterSpacing: 0.2);
+  }
+
+  TextStyle get button {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .button!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 14,
+        color: kNeutral3,
+        fontWeight: FontWeight.w500,
+        height: 1.71,
+        letterSpacing: 1.25);
+  }
+
+  TextStyle get caption {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .caption!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 12,
+        color: kNeutral3,
+        fontWeight: FontWeight.bold,
+        height: 1.5,
+        letterSpacing: 0.4);
+  }
+
+  TextStyle get overline {
+    return GoogleFonts.inter(
+        textStyle: Theme
+            .of(localContext)
+            .textTheme
+            .overline!
+            .copyWith(leadingDistribution: TextLeadingDistribution.even),
+        fontSize: 10,
+        color: kNeutral3,
+        fontWeight: FontWeight.w500,
+        height: 1.6,
+        letterSpacing: 0.5);
+  }
+}
